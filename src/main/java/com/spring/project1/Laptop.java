5bbde29682bb162d84b261559ca6ef859f198353
krishnendra007 <krishnendrasharma@gmail.com>
2024-05-17 22:50:15 +0530

    springboot commit

---
+++ src/main/java/com/spring/project1/Laptop.java
@@ -0,0 +1,12 @@
+package com.spring.project1;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Laptop {
+
+    public void compile() {
+       System.out.println("Compiling in Laptop");
+    }
+
+}
